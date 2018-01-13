d463a437430e85c1a7096e57e5067dade70a3c1e
emily <yuuamelie@gmail.com>
2018-01-13 05:35:50 -0800

    first commit

--- Cold Call/ViewController.swift
+++ Cold Call/ViewController.swift
@@ -9,7 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var num: UILabel!
+    
+    let nameBank = [
+        ("Anna"),("Bnna"),("Cnna"),("Dnna"),("Enna"),("Fnna"),("Gnna"),("Hnna"),
+    ]
+    let numberBank = [1,2,3,4,5]
+//    let number = Int(arc4random_uniform(5) + 1)
+    var x = 0
+    var y = 0
+    @IBAction func coldcallButtonPress(_ sender: UIButton) {
+        x = Int(arc4random_uniform(UInt32(nameBank.count)))
+        name.text = nameBank[x]
+        y = Int(arc4random_uniform(UInt32(numberBank.count)))
+        num.text = String(numberBank[y])
+        if num.text == "1" {
+            num.textColor = UIColor.red
+        }else if  num.text == "2" {
+            num.textColor = UIColor.green
+        }else if  num.text == "3" {
+            num.textColor = UIColor.blue
+        }else if num.text == "4" {
+            num.textColor = UIColor.purple
+        }else{
+            num.textColor = UIColor.yellow
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +47,5 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
-}
+    }
 
